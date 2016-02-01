6c44ccdc689a4e2e3f119486d5887dc8bddf0279
asuslennikov <anton.external@gmail.com>
2016-02-01 20:07:39 +0300

    Filter tags

--- asana4j-api/src/main/java/ru/jewelline/asana4j/api/clients/TagApiClient.java
+++ asana4j-api/src/main/java/ru/jewelline/asana4j/api/clients/TagApiClient.java
@@ -1,5 +1,6 @@
 package ru.jewelline.asana4j.api.clients;
 
+import ru.jewelline.asana4j.api.PagedList;
 import ru.jewelline.asana4j.api.clients.modifiers.RequestModifier;
 import ru.jewelline.asana4j.api.entity.Tag;
 
@@ -23,9 +24,20 @@ public interface TagApiClient {
      *
      * @return A builder which allows for user to specify parameters for the new tag and complete the creation process
      * (workspace id is already set, see {@link ru.jewelline.asana4j.api.entity.Tag.TagCreator#setWorkspace(long)}).
-     * @api.link @api.link <a href="https://asana.com/developers/api-reference/tags#create">Create a tag</a>
+     * @api.link <a href="https://asana.com/developers/api-reference/tags#create">Create a tag</a>
      * @see Tag
      * @see ru.jewelline.asana4j.api.entity.Tag.TagCreator
      */
     Tag.TagCreator createTag(long workspaceId);
+
+    /**
+     * Returns the compact tag records for some filtered set of tags
+     * <p><i>Triggers HTTP communication with server</i></p>
+     *
+     * @param filter           A filter with specified search criterias
+     * @param requestModifiers Additional request modifiers such as pagination, requested fields and so on.
+     * @return Returns the compact tag records for some filtered set of tags
+     * @api.link <a href="https://asana.com/developers/api-reference/tags#query">Query for tags</a>
+     */
+    PagedList<Tag> getTags(TagFilter filter, RequestModifier... requestModifiers);
 }
--- asana4j-core/src/main/java/ru/jewelline/asana4j/core/impl/api/clients/TagApiClientImpl.java
+++ asana4j-core/src/main/java/ru/jewelline/asana4j/core/impl/api/clients/TagApiClientImpl.java
@@ -1,13 +1,17 @@
 package ru.jewelline.asana4j.core.impl.api.clients;
 
+import ru.jewelline.asana4j.api.PagedList;
 import ru.jewelline.asana4j.api.clients.TagApiClient;
+import ru.jewelline.asana4j.api.clients.TagFilter;
 import ru.jewelline.asana4j.api.clients.modifiers.RequestModifier;
 import ru.jewelline.asana4j.api.entity.Tag;
 import ru.jewelline.asana4j.api.entity.io.EntityDeserializer;
+import ru.jewelline.asana4j.api.entity.io.SerializableEntity;
 import ru.jewelline.asana4j.core.impl.api.RequestFactory;
 import ru.jewelline.asana4j.core.impl.api.entity.TagImpl;
 import ru.jewelline.asana4j.core.impl.api.entity.TagImplCreator;
 import ru.jewelline.asana4j.core.impl.api.entity.common.ApiEntityContext;
+import ru.jewelline.asana4j.core.impl.api.entity.io.SimpleFieldsUpdater;
 import ru.jewelline.asana4j.http.HttpMethod;
 
 public class TagApiClientImpl extends ApiClientImpl implements TagApiClient {
@@ -22,7 +26,7 @@ public class TagApiClientImpl extends ApiClientImpl implements TagApiClient {
 
     @Override
     public Tag getTagById(long tagId, RequestModifier... requestModifiers) {
-        return apiRequest()
+        return apiRequest(requestModifiers)
                 .path("tags/" + tagId)
                 .buildAs(HttpMethod.GET)
                 .execute()
@@ -32,5 +36,29 @@ public class TagApiClientImpl extends ApiClientImpl implements TagApiClient {
     @Override
     public Tag.TagCreator createTag(long workspaceId) {
         return new TagImplCreator(getEntityContext()).setWorkspace(workspaceId);
+    }
+
+    @Override
+    public PagedList<Tag> getTags(TagFilter filter, RequestModifier... requestModifiers) {
+        return apiRequest(requestModifiers)
+                .path("tags")
+                .setEntity(convertFromTagFilter(filter))
+                .buildAs(HttpMethod.GET)
+                .execute()
+                .asApiCollection(getTagDeserializer());
+    }
+
+    private SerializableEntity convertFromTagFilter(TagFilter filter){
+        SimpleFieldsUpdater updater = new SimpleFieldsUpdater();
+        if (filter != null) {
+            if (filter.getWorkspaceId() > 0) {
+                updater.setField("workspace", filter.getWorkspaceId());
+            }
+            if (filter.getTeamId() > 0) {
+                updater.setField("team", filter.getTeamId());
+            }
+            updater.setField("archived", filter.isArchived());
+        }
+        return updater.wrapFieldsAsEntity();
     }
 }
