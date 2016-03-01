282dab39510ccf0cc76dea26f474915402fb5904
Anton Suslennikov <antsusl@mera.ru>
2016-03-01 13:15:27 +0300

    Fix test for OpenJDK 7

--- http-request/src/test/java/ru/jewelline/request/http/impl/UrlBuilderImplTest.java
+++ http-request/src/test/java/ru/jewelline/request/http/impl/UrlBuilderImplTest.java
@@ -46,7 +46,11 @@ public class UrlBuilderImplTest {
         assertThat(getUrlBuilder()
                 .path("http://example.com")
                 .setQueryParameter("p1", "v1", "v2", "v3")
-                .build()).isEqualTo("http://example.com?p1=v1&p1=v2&p1=v3");
+                .build())
+                .contains("http://example.com?")
+                .contains("p1=v1")
+                .contains("p1=v2")
+                .contains("p1=v3");
     }
 
     @Test
@@ -55,7 +59,11 @@ public class UrlBuilderImplTest {
                 .path("http://example.com")
                 .setQueryParameter("p1", "v1")
                 .setQueryParameter("p2", "v2")
-                .build()).isEqualTo("http://example.com?p1=v1&p2=v2");
+                .build())
+                .contains("http://example.com?")
+                .contains("p1=v1")
+                .contains("p2=v2")
+        ;
     }
 
     @Test
@@ -65,7 +73,10 @@ public class UrlBuilderImplTest {
                 .setQueryParameter("p1", "v1")
                 .setQueryParameter("p2", null)
                 .setQueryParameter("p3", "v3")
-                .build()).isEqualTo("http://example.com?p1=v1&p3=v3");
+                .build())
+                .contains("http://example.com?")
+                .contains("p1=v1")
+                .contains("p3=v3");
     }
 
     @Test
@@ -75,7 +86,10 @@ public class UrlBuilderImplTest {
                 .setQueryParameter("p!@#$%^&*()_+1", "v 1")
                 .setQueryParameter("p2", "?!@#$%^&*%28%29_+:")
                 .build();
-        assertThat(url).isEqualTo("http://example.com?p2=%3F%21%40%23%24%25%5E%26*%2528%2529_%2B%3A&p%21%40%23%24%25%5E%26*%28%29_%2B1=v%201");
+        assertThat(url).contains("http://example.com?")
+                .contains("p2=%3F%21%40%23%24%25%5E%26*%2528%2529_%2B%3A")
+                .contains("p%21%40%23%24%25%5E%26*%28%29_%2B1=v%201");
+
         try {
             new URL(url);
         } catch (MalformedURLException e) {
